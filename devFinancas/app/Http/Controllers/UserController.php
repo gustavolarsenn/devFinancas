ccae87d3c86ea39952f7dfe84d371f97869bef18
gustavolarsenn <william_gl02@hotmail.com>
2024-04-09 20:17:18 -0300

    Usando Laravel

---
+++ devFinancas/app/Http/Controllers/UserController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required',
+            'pasword' => 'required'
+          ]);
+          User::create($request->all());
+          return redirect()->route('users.index')
+            ->with('success','User created successfully.');
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $user = User::find($id);
+        return view('users.show', compact('user'));
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $request->validate([
+            'name' => 'required|max:255',
+            'email' => 'required',
+            'password' => 'required'
+          ]);
+          $user = USer::find($id);
+          $user->update($request->all());
+          return redirect()->route('users.index')
+            ->with('success', 'User updated successfully.');
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $user = User::find($id);
+        $user->delete();
+        return redirect()->route('users.index')
+          ->with('success', 'User deleted successfully');
+        //
+    }
+
+    // routes functions
+    /**
+     * Show the form for creating a new post.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('users.create');
+    }
+}
