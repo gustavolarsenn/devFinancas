078814382b710929c6566b2a1ac0b3a629de2537
gustavolarsenn <william_gl02@hotmail.com>
2024-05-30 20:25:19 -0300

    Mudança tabela categoria

--- sql/category.sql
+++ sql/category.sql
@@ -1,4 +1,14 @@
 CREATE TABLE categories (
     category_id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    category_name VARCHAR(255)
+    category_name VARCHAR(255),
+    user_id BIGINT,
 );
+
+INSERT INTO categories (category_name, user_id) VALUES 
+('Alimentação', 0),
+('Transporte', 0),
+('Moradia', 0),
+('Saúde', 0),
+('Educação', 0),
+('Lazer', 0),
+('Esporte', 0);
